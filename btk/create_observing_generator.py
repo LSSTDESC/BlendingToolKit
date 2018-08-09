1d4f122a0d0cc7ad99ac43e3b84dc5f9b33c713b
sowmyakth <sowmya.spacebug@gmail.com>
2018-08-09 09:50:36 -0700

    returns generator with list of observing conditions

--- btk/create_observing_generator.py
+++ btk/create_observing_generator.py
@@ -1,22 +1,15 @@
+"""Creates a generator of characteristics of a given observation for a given
+survey.
+"""
 import descwl
-import argparse
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    survey_group = parser.add_argument_group('Survey parameters',
-        'Specify survey camera and observing parameters.')
-    descwl.survey.Survey.add_args(survey_group)
-    args = parser.parse_args()
-    keys = ['exposure_time', 'cosmic_shear_g2', 'image_width', 'filter_band',
-            'image_height', 'zenith_psf_fwhm', 'survey_name']
-    kys2 = ['exposure-time', 'cosmic-shear-g2', 'image-width', 'filter-band',
-            'image-height', 'zenith-psf-fwhm', 'survey-name']
-    survey = descwl.survey.Survey.from_args(args)
-    for i, key in enumerate(keys):
-        print(str(kys2[i]), str(vars(survey)[key]))
-
-
-if __name__ == "__main__":
-    main()
+def generate(Args):
+    while True:
+        if Args.survey_name is 'LSST':
+            observing_generator = []
+            for band in Args.bands:
+                survey = descwl.survey.Survey.get_defaults(survey_name='LSST',
+                                                           filter_band=band)
+                observing_generator.append(survey)
+        yield observing_generator
