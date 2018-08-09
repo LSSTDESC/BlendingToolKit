316852debe85969483f03d6a1d830ca048e7b211
sowmyakth <sowmya.spacebug@gmail.com>
2018-08-08 22:35:09 -0700

    script to load actalog added

---
+++ btk/create_blend_generator.py
@@ -0,0 +1,4 @@
+"""The generator creates blend lists according to a given strategy"""
+
+
+def generate(Args):
---
+++ btk/get_input_catalog.py
@@ -0,0 +1,21 @@
+"""Function creates an astropy table containing information that is useful to
+generate postage stamp images with appropriate distributions of shapes, colors,
+fluxes, etc.
+TODO:
+1) Add script to load DC2 catalog
+2) Add option to load multiple catlogs(eg star , galaxy)
+"""
+import os
+import astropy.table
+
+
+def load_catlog(Args):
+    """Returns astropy table with input name"""
+    name, ext = os.path.splitext(Args.catalog_name)
+    if ext == '.fits':
+        table = astropy.table.Table.read(Args.catalog_name,
+                                         format='fits')
+    else:
+        table = astropy.table.Table.read(Args.catalog_name,
+                                         format='ascii.basic')
+    return table
