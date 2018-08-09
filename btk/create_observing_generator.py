cc4a9cd28dad4a5da0dd8b084196205cafbdcc57
sowmyakth <sowmya.spacebug@gmail.com>
2018-08-09 14:44:48 -0700

    image size set using input stamp_size

--- btk/create_observing_generator.py
+++ btk/create_observing_generator.py
@@ -6,12 +6,14 @@ import descwl
 
 def generate(Args):
     while True:
-        if Args.survey_name is 'LSST':
-            observing_generator = []
-            for band in Args.bands:
-                survey = descwl.survey.Survey.get_defaults(survey_name='LSST',
-                                                           filter_band=band)
-                descwl_survey = descwl.survey.Survey(survey_name='LSST',
-                                                     filter_band='u', **survey)
-                observing_generator.append(descwl_survey)
-        yield observing_generator
+        observing_generator = []
+        for band in Args.bands:
+            survey = descwl.survey.Survey.get_defaults(
+                survey_name=Args.survey_name,
+                filter_band=band)
+            survey['image_width'] = Args.stamp_size / survey['pixel_scale']
+            survey['image_height'] = Args.stamp_size / survey['pixel_scale']
+            descwl_survey = descwl.survey.Survey(survey_name=Args.survey_name,
+                                                 filter_band='u', **survey)
+            observing_generator.append(descwl_survey)
+    yield observing_generator
