f0d1d083e3b72375aadf5feda95e15d71dd97497
sowmyakth <sowmya.spacebug@gmail.com>
2018-10-29 19:28:59 -0700

    measure.py runs for basic stack and scarlet in utils

--- btk/utils.py
+++ btk/utils.py
@@ -9,26 +9,29 @@ import lsst.meas.base
 import lsst.meas.deblender
 import lsst.meas.extensions.shapeHSM
 import scarlet
-from measure import Measurement_params
+from btk import measure
+import numpy as np
 
 
-class Stack_params(Measurement_params):
+class Stack_params(measure.Measurement_params):
     min_pix = 1
     bkg_bin_size = 32
     thr_value = 5
 
-    def make_measurement(self, data):
-        image_array, variance_array, psf_array = data
+    def make_measurement(self, data, index):
+        """Perform detection, deblending and measurement on the i band image of
+         the blend image for input index in the batch."""
+        image_array = data['blend_images'][index, :, :, 4].astype(np.float32)
+        variance_array = image_array + data['sky_level'][index, 4]
+        psf_array = data['psf_images'][index, :, :, 4].astype(np.float64)
         cat = run_stack(image_array, variance_array, psf_array,
                         min_pix=self.min_pix, bkg_bin_size=self.bkg_bin_size,
                         thr_value=self.thr_value)
-        mask = cat['deblend_nChild'] == 0
-        mask &= cat['base_SdssCentroid_flag'] is False
-        cat_chldrn = cat[mask]
+        cat_chldrn = cat[cat['deblend_nChild'] == 0]
         cat_chldrn = cat_chldrn.copy(deep=True)
         return cat_chldrn.asAstropy()
 
-    def get_deblended_images(self, data):
+    def get_deblended_images(self, data=None, index=None):
         return None
 
 
@@ -90,14 +93,14 @@ def run_stack(image_array, variance_array, psf_array,
     return catalog
 
 
-class Scarlet_params(Measurement_params):
+class Scarlet_params(measure.Measurement_params):
     iters = 200
     e_rel = .015
 
-    def make_measurement(self, data=None):
+    def make_measurement(self, data=None, index=None):
         return None
 
-    def get_deblended_images(self, data):
+    def get_deblended_images(self, data, index):
         """
         Deblend input images with scarlet
         Args:
@@ -114,24 +117,24 @@ class Scarlet_params(Measurement_params):
         rejected_sources: list of sources (if any) that scarlet was
         unable to initlaize the image with.
         """
-        images, peaks, bg_rms = data
+        images = np.transpose(data['blend_images'][index], axes=(2, 0, 1))
+        blend_cat = data['blend_list'][index]
+        peaks = np.stack((blend_cat['dx'], blend_cat['dy']), axis=1)
+        bg_rms = data['sky_level'][index]**0.5
         blend, rejected_sources = scarlet_initialize(images, peaks,
-                                                     bg_rms)
-        sources, rejected_sources = [], []
-        for n, peak in enumerate(peaks):
-            try:
-                result = scarlet.ExtendedSource((peak[1], peak[0]), images,
-                                                bg_rms)
-                sources.append(result)
-            except scarlet.source.SourceInitError:
-                rejected_sources.append(n)
-                print("No flux in peak {0} at {1}".format(n, peak))
-        blend = scarlet.Blend(sources).set_data(images, bg_rms=bg_rms)
-        return blend, rejected_sources
+                                                     bg_rms, self.iters, self.e_rel)
+        im = []
+        for m in range(len(blend.sources)):
+            oth_indx = np.delete(range(len(blend.sources)), m)
+            model_oth = np.zeros_like(images)
+            for i in oth_indx:
+                model_oth += blend.get_model(k=i)
+            im.append(np.transpose(images - model_oth, axes=(1, 2, 0)))
+        return np.array(im)
 
 
 def scarlet_initialize(images, peaks,
-                       bg_rms):
+                       bg_rms, iters, e_rel):
     """ Intializes scarlet ExtendedSource at locations specified as peaks
     in the (multi-band) input images.
     Args:
@@ -148,11 +151,14 @@ def scarlet_initialize(images, peaks,
     sources, rejected_sources = [], []
     for n, peak in enumerate(peaks):
         try:
-            result = scarlet.ExtendedSource((peak[1], peak[0]), images,
-                                            bg_rms)
+            result = scarlet.ExtendedSource(
+                (peak[1], peak[0]),
+                images,
+                bg_rms)
             sources.append(result)
         except scarlet.source.SourceInitError:
             rejected_sources.append(n)
             print("No flux in peak {0} at {1}".format(n, peak))
     blend = scarlet.Blend(sources).set_data(images, bg_rms=bg_rms)
+    blend.fit(iters, e_rel=e_rel)
     return blend, rejected_sources
