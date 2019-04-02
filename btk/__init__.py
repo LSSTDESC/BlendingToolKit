5503d84c4758ba92c295a48cfb3156a855f2d24b
Sowmya Kamath <sowmyakth@users.noreply.github.com>
2019-04-01 18:20:09 -0700

    minor formatting

--- btk/__init__.py
+++ btk/__init__.py
@@ -1,5 +1,6 @@
 """Toolkit for fast simulation and analysis of overlapping objects for the
 LSST Dark Energy Science Collaboration.
+
 The code generates on the fly images of overlapping parametric galaxies while
 providing framework to test user defined detection/deblending/measurement
 algorithms.
