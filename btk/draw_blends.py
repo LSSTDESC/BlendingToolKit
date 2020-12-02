58878587a7b4806bfd38b9bfe9b840d4dda44c8b
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2020-12-02 14:52:24 +0100

    Make compatible_catalogs a class attribute

--- btk/draw_blends.py
+++ btk/draw_blends.py
@@ -64,6 +64,9 @@ def get_size(pixel_scale, catalog, cutout):
 
 
 class DrawBlendsGenerator(ABC):
+
+    compatible_catalogs = ("Catalog",)
+
     def __init__(
         self,
         blend_generator,
@@ -116,11 +119,6 @@ class DrawBlendsGenerator(ABC):
         self.add_noise = add_noise
         self.min_snr = min_snr
         self.verbose = verbose
-
-    @abstractmethod
-    @property
-    def compatible_catalogs(self):
-        pass
 
     def __iter__(self):
         return self
@@ -339,9 +337,7 @@ class DrawBlendsGenerator(ABC):
 
 
 class WLDGenerator(DrawBlendsGenerator):
-    @property
-    def compatible_catalogs(self):
-        return "WLDCatalog"
+    compatible_catalogs = ("WLDCatalog",)
 
     def render_single(self, entry, cutout, band):
         """Returns the Galsim Image of an isolated galaxy.
@@ -411,6 +407,8 @@ class GalsimRealDraw(DrawBlendsGenerator):
         list of arguments for the psf function
     """
 
+    compatible_catalogs = ("CosmosCatalog",)
+
     def __init__(
         self,
         cat,
@@ -425,10 +423,6 @@ class GalsimRealDraw(DrawBlendsGenerator):
         self.singles = None
         self.locs = None
         self.blend = None
-
-    @property
-    def compatible_catalogs(self):
-        return "CosmosCatalog"
 
     def draw_single(self, cat, shift):
         """Draws a single random galaxy profile in a random location of the image
