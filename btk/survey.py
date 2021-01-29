6587e2b61a06ccbd5115f08818d316342c336ff7
Ismael Mendoza <imendoza@umich.edu>
2021-01-29 14:05:26 -0500

    survey update, add correct values, keywords for clarity, delete moffat separate function

--- btk/survey.py
+++ btk/survey.py
@@ -3,16 +3,14 @@ import numpy as np
 import galsim
 from collections import namedtuple
 
-# A simple class archetype to serve as a dictionary without having to write the field names
-# every time.
 Survey = namedtuple(
     "Survey",
     [
         "name",
-        "pixel_scale",
-        "effective_area",
-        "mirror_diameter",
-        "airmass",
+        "pixel_scale",  # arcseconds per pixel
+        "effective_area",  # Effective total light collecting area in square meters [m2]
+        "mirror_diameter",  # in meters [m]
+        "airmass",  # Optical path length through atmosphere relative to zenith path length.
         "filters",
     ],
 )
@@ -21,25 +19,15 @@ Filter = namedtuple(
     "Filter",
     [
         "name",
-        "psf_scale",
-        "sky_brightness",
-        "exp_time",
-        "zenith_psf_fwhm",
-        "zero_point",
-        "extinction",
+        "atmospheric_psf_fwhm",  # in arcseconds.
+        "sky_brightness",  # mags/sq.arcsec
+        "exp_time",  # in seconds [s]
+        "zeropoint",  # in electrons per second at 24th magnitude.
+        "extinction",  # Exponential exctinction coefficient for atmospheric absorption.
     ],
 )
 
-
-pix_roman = 0.11
-pix_rubin = 0.2
-pix_hst = 0.06
-pix_euclid = 0.101
-pix_hsc = 0.167
-pix_des = 0.263
-pix_cfht = 0.185
-
-# Central wavelengths in Angstroms for each LSST filter band, calculated from the
+# Central wavelengths in Angstroms for each filter band, calculated from the
 # baseline total filter throughputs tabulated at
 # http://dev.lsstcorp.org/cgit/LSST/sims/throughputs.git/snapshot/throughputs-1.2.tar.gz
 _central_wavelength = {
@@ -50,85 +38,302 @@ _central_wavelength = {
     "z": 8689.83,
     "y": 9674.05,
     "VIS": 7135.0,
+    "f814w": 5000.0,  # TODO: placeholder
 }
 
 
 # https://sci.esa.int/documents/33859/36320/1567253682555-Euclid_presentation_Paris_1Dec2009.pdf
-Euclid = Survey("Euclid", pix_euclid, [Filter("VIS", 0.16, 22.9, 2260, 6.85)])
+# http://www.mssl.ucl.ac.uk/~smn2/instrument.html
+# area in square meters after 13% obscuration as in: https://arxiv.org/pdf/1608.08603.pdf
+# exposure time: 4 exposures combined as in Cropper et al. 2018
+# sky brightness: http://www.mssl.ucl.ac.uk/~smn2/instrument.html
+# extinction: No atmosphere
+Euclid = Survey(
+    name="Euclid",
+    pixel_scale=0.101,
+    effective_area=1.15,
+    mirror_diameter=1.3,
+    airmass=1.0,
+    zeropoint_airmass=1.0,
+    filters=[
+        Filter(
+            name="VIS",
+            atmospheric_psf_fwhm=0.17,
+            sky_brightness=22.9207,
+            exp_time=2260,
+            zeropoint=6.85,
+            extinction=0.05,
+        )
+    ],
+)
 
 # Source https://hst-docs.stsci.edu/display/WFC3IHB/6.6+UVIS+Optical+Performance#id-6
 # .6UVISOpticalPerformance-6.6.1 800nm
-HST = Survey("HST", pix_hst, [Filter("f814w", 0.074, 22, 3000, 20)])
+HST = Survey(
+    name="HST",
+    pixel_scale=0.06,
+    effective_area=1.0,  # TODO: placeholder
+    mirror_diameter=1.0,  # TODO: placeholder
+    airmass=1.0,  # TODO: double-check
+    zeropoint_airmass=1.0,  # TODO: double-check
+    filters=[
+        Filter(
+            name="f814w",
+            atmospheric_psf_fwhm=0.074,
+            sky_brightness=22,
+            exp_time=3000,
+            zeropoint=20,
+            extinction=0,  # TODO: double-check
+        )
+    ],
+)
 
 # https://hsc-release.mtk.nao.ac.jp/doc/ deep+udeep
 
 HSC = Survey(
-    "HSC",
-    pix_hsc,
-    [
-        Filter("g", 0.306, 21.4, 600, 91.11),
-        Filter("r", 0.285, 20.6, 600, 87.74),
-        Filter("i", 0.238, 19.7, 1200, 69.80),
-        Filter("z", 0.268, 18.3, 1200, 29.56),
-        Filter("y", 0.272, 17.9, 1200, 21.53),
+    name="HSC",
+    pixel_scale=0.167,
+    effective_area=52.81,
+    mirror_diameter=8.2,
+    airmass=1.0,
+    zeropoint_airmass=1.2,
+    filters=[
+        Filter(
+            name="g",
+            atmospheric_psf_fwhm=0.72,
+            sky_brightness=21.4,
+            exp_time=600,
+            zeropoint=91.11,
+            extinction=0.13,
+        ),
+        Filter(
+            name="r",
+            atmospheric_psf_fwhm=0.67,
+            sky_brightness=20.6,
+            exp_time=600,
+            zeropoint=87.74,
+            extinction=0.11,
+        ),
+        Filter(
+            name="i",
+            atmospheric_psf_fwhm=0.56,
+            sky_brightness=19.7,
+            exp_time=1200,
+            zeropoint=69.80,
+            extinction=0.07,
+        ),
+        Filter(
+            name="y",
+            atmospheric_psf_fwhm=0.64,
+            sky_brightness=18.3,
+            exp_time=1200,
+            zeropoint=29.56,
+            extinction=0.05,
+        ),
+        Filter(
+            name="z",
+            atmospheric_psf_fwhm=0.64,
+            sky_brightness=17.9,
+            exp_time=1200,
+            zeropoint=21.53,
+            extinction=0.05,
+        ),
     ],
 )
 
 # Sigma of the psf profile in arcseconds.
 # https://arxiv.org/pdf/1702.01747.pdf Z-band
 Roman = Survey(
-    "Roman",
-    pix_roman,
-    [
-        Filter("F062", 0.1848, 22, 3000, 26.99),
-        Filter("Z087", 0.1859, 22, 3000, 26.39),
-        Filter("Y106", 0.2046, 22, 3000, 26.41),
-        Filter("J129", 0.2332, 22, 3000, 26.35),
-        Filter("H158", 0.2684, 22, 3000, 26.41),
-        Filter("F184", 0.2981, 22, 3000, 25.96),
-    ],  # Mean sky level and exposure time need to be checked
+    name="Roman",
+    pixel_scale=0.11,
+    effective_area=1.0,  # TODO: placeholder
+    mirror_diameter=1.0,  # TODO: placeholder
+    airmass=1.0,  # TODO: double-check
+    zeropoint_airmass=1.0,  # TODO: double-check
+    filters=[
+        Filter(
+            name="F062",
+            atmospheric_psf_fwhm=0.1848,
+            sky_brigthness=22,  # TODO: double-check
+            exp_time=3000,  # TODO: double-check
+            zeropoint=26.99,
+        ),
+        Filter(
+            name="Z087",
+            atmospheric_psf_fwhm=0.1859,
+            sky_brigthness=22,  # TODO: double-check
+            exp_time=3000,  # TODO: double-check
+            zeropoint=26.39,
+        ),
+        Filter(
+            name="Y106",
+            atmospheric_psf_fwhm=0.2046,
+            sky_brigthness=22,  # TODO: double-check
+            exp_time=3000,  # TODO: double-check
+            zeropoint=26.41,
+        ),
+        Filter(
+            name="J129",
+            atmospheric_psf_fwhm=0.2332,
+            sky_brigthness=22,  # TODO: double-check
+            exp_time=3000,  # TODO: double-check
+            zeropoint=26.35,
+        ),
+        Filter(
+            name="H158",
+            atmospheric_psf_fwhm=0.2684,
+            sky_brigthness=22,  # TODO: double-check
+            exp_time=3000,  # TODO: double-check
+            zeropoint=26.41,
+        ),
+        Filter(
+            name="F184",
+            atmospheric_psf_fwhm=0.2981,
+            sky_brigthness=22,  # TODO: double-check
+            exp_time=3000,  # TODO: double-check
+            zeropoint=25.96,
+        ),
+    ],
 )
 
 # https://www.lsst.org/about/camera/features
 Rubin = Survey(
     "LSST",
-    pix_rubin,
-    32.4,
-    8.36,
-    1.2,
-    [
-        Filter("y", 0.327, 18.6, 4800, 0.63, 10.58, 0.138),
-        Filter("z", 0.310, 19.6, 4800, 0.65, 22.68, 0.043),
-        Filter("i", 0.297, 20.5, 5520, 0.67, 32.36, 0.07),
-        Filter("r", 0.285, 21.2, 5520, 0.70, 43.70, 0.10),
-        Filter("g", 0.276, 22.3, 2400, 0.73, 50.70, 0.163),
-        Filter("u", 0.267, 22.9, 1680, 0.77, 9.16, 0.451),
+    pixel_scale=0.2,
+    effective_area=32.4,
+    mirror_diameter=8.36,
+    airmass=1.2,
+    zeropoint_airmass=1.2,
+    filters=[
+        Filter(
+            name="y",
+            atmospheric_psf_fwhm=0.703,
+            sky_brightness=18.6,
+            exp_time=4800,
+            zeropoint=10.58,
+            extinction=0.138,
+        ),
+        Filter(
+            name="z",
+            atmospheric_psf_fwhm=0.725,
+            sky_brightness=19.6,
+            exp_time=4800,
+            zeropoint=22.68,
+            extinction=0.043,
+        ),
+        Filter(
+            name="i",
+            atmospheric_psf_fwhm=0.748,
+            sky_brightness=20.5,
+            exp_time=5520,
+            ero_point=32.36,
+            extinction=0.07,
+        ),
+        Filter(
+            name="r",
+            atmospheric_psf_fwhm=0.781,
+            sky_brightness=21.2,
+            exp_time=520,
+            zeropoint=43.70,
+            extinction=0.10,
+        ),
+        Filter(
+            name="g",
+            atmospheric_psf_fwhm=0.814,
+            sky_brightness=22.3,
+            exp_time=2400,
+            zeropoint=50.70,
+            extinction=0.163,
+        ),
+        Filter(
+            name="u",
+            atmospheric_psf_fwhm=0.859,
+            sky_brightness=2.9,
+            exp_time=1680,
+            zeropoint=9.16,
+            extinction=0.451,
+        ),
     ],
 )
+
+# http://www.ctio.noao.edu/noao/content/Basic-Optical-Parameters
+# http://www.ctio.noao.edu/noao/content/DECam-What
+# http://www.darkenergysurvey.org/survey/des-description.pdf
+# skybrightness from http://www.ctio.noao.edu/noao/node/1218
+# extinction from https://arxiv.org/pdf/1701.00502.pdf table 6
+# fwhm values from https://arxiv.org/pdf/1407.3801.pdf
 DES = Survey(
-    "DES",
-    pix_des,
-    [
-        Filter("i", None, None, None, None),
-        Filter("r", None, None, None, None),
-        Filter("g", None, None, None, None),
-        Filter("z", None, None, None, None),
-    ],  # Will not work properly until we fill in the correct values
+    name="DES",
+    pixel_scale=0.263,
+    mirror_diameter=3.934,
+    effective_area=10.014,
+    airmass=1.0,
+    zeropoint_airmass=1.3,
+    filters=[
+        Filter(
+            name="i",
+            atmospheric_psf_fwhm=0.96,
+            sky_brightness=20.5,
+            exp_time=1000,
+            zeropoint=13.94,
+            extinction=0.05,
+        ),
+        Filter(
+            name="r",
+            atmospheric_psf_fwhm=1.03,
+            sky_brightness=21.4,
+            exp_time=800,
+            zeropoint=15.65,
+            extinction=0.09,
+        ),
+        Filter(
+            name="g",
+            atmospheric_psf_fwhm=1.24,
+            sky_brightness=22.3,
+            exp_time=800,
+            zeropoint=12.29,
+            extinction=0.17,
+        ),
+        Filter(
+            name="z",
+            atmospheric_psf_fwhm=1.12,
+            sky_brightness=18.7,
+            exp_time=800,
+            zeropoint=10.81,
+            extinction=0.06,
+        ),
+    ],
 )
+
+# http://www.cfht.hawaii.edu/Instruments/Imaging/Megacam/generalinformation.html
+# http://www.cfht.hawaii.edu/Instruments/ObservatoryManual/om-focplndat.gif
+# Calculating zeropoints with:
+# http://www1.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/community/CFHTLS-SG/docs/extra/filters.html
 CFHT = Survey(
-    "CFHT",
-    pix_cfht,
-    [
-        Filter("i", None, None, None, None),
-        Filter("r", None, None, None, None),
-    ],  # Will not work properly until we fill in the correct values
+    name="CFHT",
+    pixel_scale=0.185,
+    mirror_diameter=3.592,
+    effective_area=8.022,
+    airmass=1.0,  # TODO: double-check
+    zeropoint_airmass=1.0,  # TODO: double-check
+    filters=[
+        Filter(
+            name="i",
+            atmospheric_psf_fwhm=0.64,
+            sky_brightness=20.3,
+            exp_time=4300,
+            zeropoint=8.46,
+            extinction=0.07,
+        ),
+        Filter(
+            name="r",
+            atmospheric_psf_fwhm=0.71,
+            sky_brightness=20.8,
+            zeropoint=10.72,
+            extinction=0.10,
+        ),
+    ],
 )
-
-
-def get_filter(survey, band):
-    bands = [filt.name for filt in survey.filters]
-    assert band in bands
-    return survey.filters[bands.index(band)]
 
 
 def get_flux(ab_magnitude, filt, survey):
@@ -141,16 +346,8 @@ def get_flux(ab_magnitude, filt, survey):
     Returns:
         float: Flux in detected electrons.
     """
-    zeropoint_airmass = 1.0
-    if survey.name == "DES":
-        zeropoint_airmass = 1.3
-    if survey.name == "LSST" or survey.name == "HSC":
-        zeropoint_airmass = 1.2
-    if survey.name == "Euclid":
-        zeropoint_airmass = 1.0
-
-    mag = ab_magnitude + filt.extinction * (filt.airmass - zeropoint_airmass)
-    return filt.exposure_time * filt.zero_point * 10 ** (-0.4 * (mag - 24))
+    mag = ab_magnitude + filt.extinction * (filt.airmass - filt.zeropoint_airmass)
+    return filt.exposure_time * filt.zeropoint * 10 ** (-0.4 * (mag - 24))
 
 
 def get_mean_sky_level(survey, filt):
@@ -183,44 +380,23 @@ def make_wcs(pixel_scale, shape, center_pix=None, center_sky=None, projection="T
     return w
 
 
-def get_moffat_psf(survey, filt, psf_stamp_size):
-    """Generates a psf as a Galsim object using the survey information"""
-    assert psf_stamp_size % 2 == 1
-
-    def psf_function(r):
-        return galsim.Moffat(2, r)
-
-    psf_obj = psf_function(filt.psf_scale).withFlux(1.0)
-    psf = psf_obj.drawImage(
-        nx=psf_stamp_size,
-        ny=psf_stamp_size,
-        method="no_pixel",
-        use_true_center=True,
-        scale=survey.pixel_scale,
-    ).array
-
-    # Make sure PSF vanishes on the edges of a patch that
-    # has the shape of the initial psf
-    psf = psf - psf[1, int(psf_stamp_size / 2)] * 2
-    psf[psf < 0] = 0
-    psf /= np.sum(psf)
-
-    # Generating an unintegrated galsim psf for the convolution
-    psf_obj = galsim.InterpolatedImage(
-        galsim.Image(psf), scale=survey.pixel_scale
-    ).withFlux(1.0)
-
-    return psf_obj
-
-
-def get_psf(survey):
+def get_psf(filt, atmospheric_model="Kolmogorov"):
     """Credit: WeakLensingDeblending (https://github.com/LSSTDESC/WeakLensingDeblending)"""
-    psfs = []  # one per filter.
-    for filt in survey.filters:
 
-        # get optical psf
-        assert filt.mirror_diameter > 0
-        area_ratio = filt.effective_area / (np.pi * (0.5 * filt.mirror_diameter) ** 2)
+    # get atmospheric psf
+    if atmospheric_model == "Kolmogorov":
+        atmospheric_psf_model = galsim.Kolmogorov(fwhm=filt.atmospheric_psf_fwhm)
+    elif atmospheric_model == "Moffat":
+        atmospheric_psf_model = galsim.Moffat(2, fwhm=filt.atmospheric_psf_fwhm)
+    else:
+        raise NotImplementedError(
+            f"The atmospheric model request '{atmospheric_model}' is incorrect or not implemented."
+        )
+
+    # get optical psf if available
+    if filt.mirror_diameter > 0:
+        mirror_area = np.pi * (0.5 * filt.mirror_diameter) ** 2
+        area_ratio = filt.effective_area / mirror_area
         if area_ratio <= 0 or area_ratio > 1:
             raise RuntimeError(
                 "Incompatible effective-area and mirror-diameter values."
@@ -232,12 +408,11 @@ def get_psf(survey):
         optical_psf_model = galsim.Airy(
             lam_over_diam=lambda_over_diameter, obscuration=obscuration_fraction
         )
+        psf_model = galsim.Convolve(atmospheric_psf_model, optical_psf_model).withFlux(
+            1.0
+        )
 
-        # get atmospheric psf
-        atmospheric_psf_fwhm = filt.zenith_psf_fwhm * filt.airmass ** 0.6
-        atmospheric_psf_model = galsim.Kolmogorov(fwhm=atmospheric_psf_fwhm)
+    else:
+        psf_model = atmospheric_model.withFlux(1.0)
 
-        # combine them and obtain psf image.
-        psf_model = galsim.Convolve(atmospheric_psf_model, optical_psf_model)
-        psfs.append(psf_model)
-    return psfs
+    return psf_model
