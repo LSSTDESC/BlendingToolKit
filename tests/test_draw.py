ce3147563668611f8fb15353725444c4538a330f
Ismael Mendoza <imendoza@umich.edu>
2021-03-09 08:41:34 -0500

    try again

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -151,9 +151,11 @@ class TestBasicDraw:
         default_draw_generator = get_draw_generator(fixed_parameters=True)
         draw_output = next(default_draw_generator)
         btk.plot_utils.plot_blends(draw_output["blend_images"], draw_output["blend_list"])
+        plt.close("all")
         btk.plot_utils.plot_with_isolated(
             draw_output["blend_images"], draw_output["isolated_images"], draw_output["blend_list"]
         )
+        plt.close("all")
         assert len(draw_output["blend_list"]) == 8, "Default batch should return 8"
         assert (
             len(draw_output["blend_list"][3]) < 3
@@ -162,7 +164,6 @@ class TestBasicDraw:
         self.match_blend_images_default(draw_output["blend_images"])
         self.match_isolated_images_default(draw_output["isolated_images"])
         self.match_background_noise(draw_output["blend_images"])
-        plt.close("all")
 
     def test_basic_sampling(self):
         sampling_function = btk.sampling_functions.BasicSamplingFunction()
