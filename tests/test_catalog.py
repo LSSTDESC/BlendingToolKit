c7087df21df8279bd119243bbedf86d8a74b9940
Ismael Mendoza <imendoza@umich.edu>
2021-03-23 17:10:24 -0400

    black

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -2,7 +2,10 @@ from btk.catalog import CatsimCatalog
 from btk.catalog import CosmosCatalog
 
 CATALOG_PATH = "data/sample_input_catalog.fits"
-COSMOS_CATALOG_PATHS = ["data/real_galaxy_catalog_25.2.fits", "data/real_galaxy_catalog_25.2_fits.fits"]
+COSMOS_CATALOG_PATHS = [
+    "data/real_galaxy_catalog_25.2.fits",
+    "data/real_galaxy_catalog_25.2_fits.fits",
+]
 
 
 def test_reading_catsim_catalog():
@@ -10,7 +13,7 @@ def test_reading_catsim_catalog():
 
     catsim_catalog = CatsimCatalog.from_file(CATALOG_PATH)
     return catsim_catalog
-    
+
 
 def test_reading_cosmos_catalog():
     """Returns the cosmos catalog"""
@@ -18,7 +21,7 @@ def test_reading_cosmos_catalog():
     cosmos_catalog = CosmosCatalog.from_file(COSMOS_CATALOG_PATHS)
     return cosmos_catalog
 
-    
+
 def test_verbose():
     # for coverage.
     CatsimCatalog.from_file(CATALOG_PATH, verbose=True)
