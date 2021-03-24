a2c97b4ee22a6d61d8993d3aa5ceebc886e80fa9
Thomas Sainrat <thomas.sainrat@student-cs.fr>
2021-03-24 11:18:39 +0000

    Added a test for galsim_hub

---
+++ tests/test_galsim_hub.py
@@ -0,0 +1,29 @@
+import btk.draw_blends
+import btk.survey
+
+COSMOS_CATALOG_PATHS = [
+    "data/real_galaxy_catalog_25.2.fits",
+    "data/real_galaxy_catalog_25.2_fits.fits",
+]
+
+
+def test_draw_galsim_hub():
+    stamp_size = 24.0
+    batch_size = 2
+    catalog = btk.catalog.CosmosCatalog.from_file(COSMOS_CATALOG_PATHS)
+    sampling_function = btk.sampling_functions.DefaultSamplingGalsimHub(stamp_size=stamp_size)
+
+    draw_generator = btk.draw_blends.GalsimHubGenerator(
+        catalog,
+        sampling_function,
+        [btk.survey.HST],
+        batch_size=batch_size,
+        stamp_size=stamp_size,
+        multiprocessing=False,
+        cpus=1,
+        add_noise=True,
+        verbose=True,
+        meas_bands=["f814w"],
+    )
+
+    _ = next(draw_generator)
