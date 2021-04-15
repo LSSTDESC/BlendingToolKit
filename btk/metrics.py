19fa6aa16df7b3bd814f5252f930f0f14f97fc14
Thomas Sainrat <thomas.sainrat@student-cs.fr>
2021-04-15 16:17:14 +0100

    Fixed issues, added statistics per galaxy and basic plotting function

--- btk/metrics.py
+++ btk/metrics.py
@@ -19,7 +19,7 @@ def get_detection_match(true_table, detected_table):
     match_table = astropy.table.Table()
     if len(detected_table) == 0 or len(true_table) == 0:
         # No match since either no detection or no true objects
-        return
+        return None
     t_x = true_table["x_peak"] - detected_table["x_peak"][:, np.newaxis]
     t_y = true_table["y_peak"] - detected_table["y_peak"][:, np.newaxis]
     dist = np.hypot(t_x, t_y)
@@ -48,23 +48,28 @@ def detection_metrics(blended_images, isolated_images, blend_list, detection_cat
     recall = []
     f1 = []
     for i in range(len(blend_list)):
-        matches_blend = matches[i]["match_detected_id"]
-        true_pos = 0
-        false_pos = 0
-        false_neg = 0
-        for match in matches_blend:
-            if match == -1:
-                false_neg += 1
+        if matches[i] is not None:
+            matches_blend = matches[i]["match_detected_id"]
+            true_pos = 0
+            false_pos = 0
+            false_neg = 0
+            for match in matches_blend:
+                if match == -1:
+                    false_neg += 1
+                else:
+                    true_pos += 1
+            for j in range(len(detection_catalogs[i])):
+                if j not in matches_blend:
+                    false_pos += 1
+            precision.append(true_pos / (true_pos + false_pos))
+            recall.append(true_pos / (true_pos + false_neg))
+            if precision[-1] != 0 and recall[-1] != 0:
+                f1.append(2 / (1 / precision[-1] + 1 / recall[-1]))
             else:
-                true_pos += 1
-        for j in range(len(detection_catalogs[i])):
-            if j not in matches_blend:
-                false_pos += 1
-        precision.append(true_pos / (true_pos + false_pos))
-        recall.append(true_pos / (true_pos + false_neg))
-        if precision[-1] != 0 and recall[-1] != 0:
-            f1.append(2 / (1 / precision[-1] + 1 / recall[-1]))
+                f1.append(0)
         else:
+            precision.append(0)
+            recall.append(0)
             f1.append(0)
     results_detection["precision"] = precision
     results_detection["recall"] = recall
@@ -84,18 +89,24 @@ def segmentation_metrics(
     results_segmentation = {}
     iou_results = []
     for i in range(len(blend_list)):
-        iou_blend_results = []
-        matches_blend = matches[i]["match_detected_id"]
-        for j, match in enumerate(matches_blend):
-            # TODO : put a correct threshold (according to noise ?)
-            threshold = 1.0
-            true_segmentation = isolated_images[i][j][meas_band_num] > threshold
-            detected_segmentation = segmentations[i][match]
-            iou_blend_results.append(
-                np.sum(np.logical_and(true_segmentation, detected_segmentation))
-                / np.sum(np.logical_or(true_segmentation, detected_segmentation))
-            )
-        iou_results.append(iou_blend_results)
+        if matches[i] is not None:
+            iou_blend_results = []
+            matches_blend = matches[i]["match_detected_id"]
+            for j, match in enumerate(matches_blend):
+                # TODO : put a correct threshold (according to noise ?)
+                if match != -1:
+                    threshold = 1.0
+                    true_segmentation = isolated_images[i][j][meas_band_num] > threshold
+                    detected_segmentation = segmentations[i][match]
+                    iou_blend_results.append(
+                        np.sum(np.logical_and(true_segmentation, detected_segmentation))
+                        / np.sum(np.logical_or(true_segmentation, detected_segmentation))
+                    )
+                else:
+                    iou_blend_results.append(-1)
+            iou_results.append(iou_blend_results)
+        else:
+            iou_results.append([-1 for j in range(len(blend_list[i]))])
     results_segmentation["iou"] = iou_results
     return results_segmentation
 
@@ -108,33 +119,43 @@ def reconstruction_metrics(
     psnr_results = []
     ssim_results = []
     for i in range(len(blend_list)):
-        mse_blend_results = []
-        psnr_blend_results = []
-        ssim_blend_results = []
-        for j in range(len(blend_list[i])):
-            match_detected = matches[i]["match_detected_id"][j]
-            mse_blend_results.append(
-                skimage.metrics.mean_squared_error(
-                    isolated_images[i][j], deblended_images[i][match_detected]
-                )
-            )
-            psnr_blend_results.append(
-                skimage.metrics.peak_signal_noise_ratio(
-                    isolated_images[i][j],
-                    deblended_images[i][match_detected],
-                    data_range=np.max(isolated_images[i][j]),
-                )
-            )
-            ssim_blend_results.append(
-                skimage.metrics.structural_similarity(
-                    np.moveaxis(isolated_images[i][j], 0, -1),
-                    np.moveaxis(deblended_images[i][match_detected], 0, -1),
-                    multichannel=True,
-                )
-            )
-        mse_results.append(mse_blend_results)
-        psnr_results.append(psnr_blend_results)
-        ssim_results.append(ssim_blend_results)
+        if matches[i] is not None:
+            mse_blend_results = []
+            psnr_blend_results = []
+            ssim_blend_results = []
+            for j in range(len(blend_list[i])):
+                match_detected = matches[i]["match_detected_id"][j]
+                if match_detected != -1:
+                    mse_blend_results.append(
+                        skimage.metrics.mean_squared_error(
+                            isolated_images[i][j], deblended_images[i][match_detected]
+                        )
+                    )
+                    psnr_blend_results.append(
+                        skimage.metrics.peak_signal_noise_ratio(
+                            isolated_images[i][j],
+                            deblended_images[i][match_detected],
+                            data_range=np.max(isolated_images[i][j]),
+                        )
+                    )
+                    ssim_blend_results.append(
+                        skimage.metrics.structural_similarity(
+                            np.moveaxis(isolated_images[i][j], 0, -1),
+                            np.moveaxis(deblended_images[i][match_detected], 0, -1),
+                            multichannel=True,
+                        )
+                    )
+                else:
+                    mse_blend_results.append(-1)
+                    psnr_blend_results.append(-1)
+                    ssim_blend_results.append(-1)
+            mse_results.append(mse_blend_results)
+            psnr_results.append(psnr_blend_results)
+            ssim_results.append(ssim_blend_results)
+        else:
+            mse_results.append([-1 for j in range(len(blend_list[i]))])
+            psnr_results.append([-1 for j in range(len(blend_list[i]))])
+            ssim_results.append([-1 for j in range(len(blend_list[i]))])
     results_reconstruction["mse"] = mse_results
     results_reconstruction["psnr"] = psnr_results
     results_reconstruction["ssim"] = ssim_results
@@ -179,6 +200,25 @@ def compute_metrics(
             deblended_images,
             matches,
         )
+    names = blend_list[0].colnames
+    if "reconstruction" in use_metrics:
+        names += ["mse", "psnr", "ssim"]
+    if "segmentation" in use_metrics:
+        names += ["iou"]
+    results["galaxy_summary"] = astropy.table.Table(names=names)
+    for i, blend in enumerate(blend_list):
+        for j, gal in enumerate(blend):
+            row = astropy.table.Table(gal)
+            if "reconstruction" in use_metrics:
+                row.add_columns([[0.0], [0.0], [0.0]], names=["mse", "psnr", "ssim"])
+                row["mse"] = results["reconstruction"]["mse"][i][j]
+                row["psnr"] = results["reconstruction"]["psnr"][i][j]
+                row["ssim"] = results["reconstruction"]["ssim"][i][j]
+            if "segmentation" in use_metrics:
+                row.add_column([0.0], name="iou")
+                row["iou"] = results["segmentation"]["iou"][i][j]
+            results["galaxy_summary"].add_row(row[0])
+
     return results
 
 
