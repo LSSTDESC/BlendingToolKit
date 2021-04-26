3ef608f5a41a9eda8ffdebb24996d01f83da6b95
thuiop <thuiop@hotmail.fr>
2021-04-26 11:55:35 +0100

    Added execution test for metrics generator

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -3,6 +3,51 @@ import numpy as np
 import btk.metrics
 
 
+def get_metrics_generator(meas_function, cpus=1):
+    """Returns draw generator with group sampling function"""
+
+    np.random.seed(0)
+    catalog_name = "data/sample_input_catalog.fits"
+    stamp_size = 24
+    survey = btk.survey.Rubin
+    shifts = [
+        [[-0.3, 1.2], [-1.6, -1.7]],
+        [[-1.1, -2.1], [1.4, 1.8]],
+        [[-1.8, -0.8], [-0.6, 2.2]],
+        [[-2.0, -0.7], [-2.2, 1.9]],
+        [[1.1, -1.5], [0.1, -2.3]],
+        [[-2.3, 1.9], [0.4, -1.9]],
+        [[2.0, -2.0], [2.0, 0.1]],
+        [[0.2, 2.4], [-1.8, -2.0]],
+    ]
+    indexes = [[4, 5], [9, 1], [9, 2], [0, 2], [3, 8], [0, 7], [10, 2], [0, 10]]
+    catalog = btk.catalog.CatsimCatalog.from_file(catalog_name)
+    draw_blend_generator = btk.draw_blends.CatsimGenerator(
+        catalog,
+        btk.sampling_functions.DefaultSampling(),
+        [survey],
+        shifts=shifts,
+        indexes=indexes,
+        stamp_size=stamp_size,
+    )
+    meas_generator = btk.measure.MeasureGenerator(
+        meas_function,
+        draw_blend_generator,
+        cpus=cpus,
+    )
+    metrics_generator = btk.metrics.MetricsGenerator(
+        meas_generator,
+        use_metrics=("detection", "segmentation", "reconstruction"),
+        target_meas={"ellipticity": btk.metrics.meas_ksb_ellipticity},
+    )
+    return metrics_generator
+
+
+def test_sep_metrics():
+    meas_generator = get_metrics_generator(btk.measure.sep_measure)
+    _, _, results = next(meas_generator)
+
+
 def test_detection_eff_matrix():
     """Tests detection efficiency matrix computation in utils by inputting a
     summary table with 4 entries, with number of true sources between 1-4 and
