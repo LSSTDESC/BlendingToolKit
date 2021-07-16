5559e49af0ffd750de8a8796a16d203597164074
mpaillassa <mpaillassa@gmail.com>
2021-07-16 19:02:49 +0900

    adding tests

--- tests/test_cosmos.py
+++ tests/test_cosmos.py
@@ -10,11 +10,37 @@ COSMOS_CATALOG_PATHS = [
     str(data_dir / "cosmos/real_galaxy_catalog_23.5_example_fits.fits"),
 ]
 
+COSMOS_EXT_CATALOG_PATHS = [
+    str(data_dir / "cosmos/real_galaxy_catalog_26_extension_example.fits"),
+    str(data_dir / "cosmos/real_galaxy_catalog_26_extension_example_fits.fits"),
+]
+
 
 def test_cosmos_galaxies():
     stamp_size = 24.0
     batch_size = 2
     catalog = CosmosCatalog.from_file(COSMOS_CATALOG_PATHS)
+    sampling_function = DefaultSampling(stamp_size=stamp_size)
+    HST = get_surveys("HST")
+
+    draw_generator = CosmosGenerator(
+        catalog,
+        sampling_function,
+        HST,
+        batch_size=batch_size,
+        stamp_size=stamp_size,
+        cpus=1,
+        add_noise=True,
+        verbose=True,
+    )
+
+    _ = next(draw_generator)
+
+
+def test_cosmos_ext_galaxies():
+    stamp_size = 24.0
+    batch_size = 2
+    catalog = CosmosCatalog.from_file(COSMOS_EXT_CATALOG_PATHS)
     sampling_function = DefaultSampling(stamp_size=stamp_size)
     HST = get_surveys("HST")
 
