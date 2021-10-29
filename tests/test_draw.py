3b9a972f39a31ca90c2657e772e3f07f03183dc9
Ismael Mendoza <imendoza@umich.edu>
2021-10-29 09:51:37 -0400

    fix tests with new seed

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -107,9 +107,9 @@ class TestBasicDraw:
         the mean and std values in the batch. This is compared to the values
         measured a priori for the default input settings.
         """
-        test_batch_max = np.array([190.012, 1458.121, 8564.862, 10559.612, 9935.189, 5472.317])
-        test_batch_mean = 2.6460062605207106
-        test_batch_std = 403.3028115573917
+        test_batch_max = np.array([157.012, 1432.121, 8024.862, 10654.612, 9056.189, 5771.317])
+        test_batch_mean = 6.018752209594785
+        test_batch_std = 403.6321158841257
         batch_max = blend_images.max(axis=(0, 2, 3))
         batch_mean = blend_images.mean()
         batch_std = blend_images.std()
@@ -138,7 +138,7 @@ class TestBasicDraw:
         the r band. This is compared to the values measured a priori for the
         default input settings.
         """
-        test_batch_noise = 120937.45957946777
+        test_batch_noise = 122276.01733398438
         batch_noise = np.var(blend_images[1, 2, 0:32, 0:32])
         np.testing.assert_almost_equal(
             batch_noise,
--- tests/test_measure.py
+++ tests/test_measure.py
@@ -11,23 +11,14 @@ from btk.survey import get_surveys
 TEST_SEED = 0
 
 
-def get_meas_generator(meas_function, cpus=1, measure_kwargs=None):
+def get_meas_results(meas_function, cpus=1, measure_kwargs=None):
     """Returns draw generator with group sampling function"""
 
     catalog_name = data_dir / "sample_input_catalog.fits"
     stamp_size = 24
     survey = get_surveys("Rubin")
-    shifts = [
-        [[-0.3, 1.2], [-1.6, -1.7]],
-        [[-1.1, -2.1], [1.4, 1.8]],
-        [[-1.8, -0.8], [-0.6, 2.2]],
-        [[-2.0, -0.7], [-2.2, 1.9]],
-        [[1.1, -1.5], [0.1, -2.3]],
-        [[-2.3, 1.9], [0.4, -1.9]],
-        [[2.0, -2.0], [2.0, 0.1]],
-        [[0.2, 2.4], [-1.8, -2.0]],
-    ]
-    indexes = [[4, 5], [9, 1], [9, 2], [0, 2], [3, 8], [0, 7], [10, 2], [0, 10]]
+    shifts = [[-0.3, 1.2]]
+    indexes = [[1]]
     catalog = CatsimCatalog.from_file(catalog_name)
     draw_blend_generator = CatsimGenerator(
         catalog,
@@ -36,48 +27,41 @@ def get_meas_generator(meas_function, cpus=1, measure_kwargs=None):
         shifts=shifts,
         indexes=indexes,
         stamp_size=stamp_size,
+        batch_size=1,
         seed=TEST_SEED,
     )
     meas_generator = MeasureGenerator(
         meas_function, draw_blend_generator, cpus=cpus, measure_kwargs=measure_kwargs
     )
-    return meas_generator
+    blend_results, results = next(meas_generator)
+    wcs = blend_results["wcs"]
+    x, y = wcs.world_to_pixel_values(shifts[0][0] / 3600, shifts[0][1] / 3600)
+    target = np.array([x.item(), y.item()])
+    return target, results
 
 
 def compare_sep():
     """Test detection with sep"""
-    meas_generator = get_meas_generator(sep_measure)
-    _, results = next(meas_generator)
+    target, results = get_meas_results(sep_measure, measure_kwargs=[{"sigma_noise": 2.0}])
     x_peak, y_peak = (
         results["catalog"]["sep_measure"][0]["x_peak"].item(),
         results["catalog"]["sep_measure"][0]["y_peak"].item(),
     )
     detected_centers = np.array([x_peak, y_peak])
-    target_detection = np.array([65.302, 51.039])
-    np.testing.assert_array_almost_equal(
-        detected_centers,
-        target_detection,
-        decimal=3,
-        err_msg="Did not get desired detections",
-    )
+    dist = np.max(np.abs(detected_centers - target))
+    np.testing.assert_array_less(dist, 1.0)
 
 
 def compare_sep_multiprocessing():
     """Test detection with sep"""
-    meas_generator = get_meas_generator(sep_measure, cpus=4)
-    _, results = next(meas_generator)
+    target, results = get_meas_results(sep_measure, measure_kwargs=[{"sigma_noise": 2.0}], cpus=4)
     x_peak, y_peak = (
         results["catalog"]["sep_measure"][0]["x_peak"].item(),
         results["catalog"]["sep_measure"][0]["y_peak"].item(),
     )
     detected_centers = np.array([x_peak, y_peak])
-    target_detection = np.array([65.302, 51.039])
-    np.testing.assert_array_almost_equal(
-        detected_centers,
-        target_detection,
-        decimal=3,
-        err_msg="Did not get desired detections",
-    )
+    dist = np.max(np.abs(detected_centers - target))
+    np.testing.assert_array_less(dist, 1.0)
 
 
 def test_algorithms():
@@ -88,19 +72,13 @@ def test_algorithms():
 
 def test_measure_kwargs():
     """Test detection with sep"""
-    meas_generator = get_meas_generator(
+    target, results = get_meas_results(
         sep_measure, measure_kwargs=[{"sigma_noise": 2.0}, {"sigma_noise": 3.0}]
     )
-    _, results = next(meas_generator)
     x_peak, y_peak = (
         results["catalog"]["sep_measure0"][0]["x_peak"].item(),
         results["catalog"]["sep_measure0"][0]["y_peak"].item(),
     )
     detected_centers = np.array([x_peak, y_peak])
-    target_detection = np.array([65.603, 51.077])
-    np.testing.assert_array_almost_equal(
-        detected_centers,
-        target_detection,
-        decimal=3,
-        err_msg="Did not get desired detections",
-    )
+    dist = np.max(np.abs(detected_centers - target))
+    np.testing.assert_array_less(dist, 1.0)
