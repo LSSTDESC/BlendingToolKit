17fd9a8590cdb3f9c1de47cfc1cbf913e55c5805
Ismael Mendoza <imendoza@umich.edu>
2021-11-14 14:39:36 -0500

    add notes to documentation for users about shifts/indexes

--- btk/create_blend_generator.py
+++ btk/create_blend_generator.py
@@ -23,9 +23,10 @@ class BlendGenerator:
             batch_size (int): Size of batches returned.
             shifts (list): Contains arbitrary shifts to be applied instead of
                            random shifts. Must be of length batch_size. Must be used
-                           with indexes.
+                           with indexes. Used mostly for internal testing purposes.
             indexes (list): Contains the ids of the galaxies to use in the stamp.
                         Must be of length batch_size. Must be used with shifts.
+                        Used mostly for internal testing purposes.
             verbose (bool): Whether to print additional information.
         """
         self.catalog = catalog
