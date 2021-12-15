59989ef4334ecb6f248bc6b3ed2e5eda5e42cfb4
thuiop <thuiop@hotmail.fr>
2021-12-15 11:37:36 +0100

    Made DefaultSampling a parent class of DefaultSamplingShear

--- btk/sampling_functions.py
+++ btk/sampling_functions.py
@@ -201,7 +201,7 @@ class BasicSampling(SamplingFunction):
         return blend_table
 
 
-class DefaultSamplingShear(SamplingFunction):
+class DefaultSamplingShear(DefaultSampling):
     """Default sampling function used for producing blend tables."""
 
     def __init__(self, max_number=2, stamp_size=24.0, maxshift=None, shear=None, seed=DEFAULT_SEED):
@@ -215,9 +215,7 @@ class DefaultSamplingShear(SamplingFunction):
             shear (tuple or None): Constant (g1,g2) shear to apply to every galaxy.
             seed (int): Seed to initialize randomness for reproducibility.
         """
-        super().__init__(max_number, seed)
-        self.stamp_size = stamp_size
-        self.maxshift = maxshift if maxshift else self.stamp_size / 10.0
+        super().__init__(max_number, stamp_size, maxshift, seed)
         self.shear = shear
 
     @property
@@ -226,52 +224,11 @@ class DefaultSamplingShear(SamplingFunction):
         return "CatsimCatalog", "CosmosCatalog"
 
     def __call__(self, table, shifts=None, indexes=None):
-        """Applies default sampling to the input CatSim-like catalog.
-
-        Returns an astropy table with entries corresponding to a blend centered close to postage
-        stamp center.
-
-        Function selects entries from input table that are brighter than 25.3 mag
-        in the i band. Number of objects per blend is set at a random integer
-        between 1 and ``self.max_number``. The blend table is then randomly sampled
-        entries from the table after selection cuts. The centers are randomly
-        distributed within 1/10th of the stamp size. Here even though the galaxies
-        are sampled from a CatSim catalog, their spatial location are not
-        representative of real blends.
-
-        Args:
-            table (Astropy.table): Table containing entries corresponding to galaxies
-                                   from which to sample.
-            shifts (list): Contains arbitrary shifts to be applied instead of random ones.
-                           Should of the form [x_peak,y_peak] where x_peak and y_peak are the lists
-                           containing the x and y shifts.
-            indexes (list): Contains the indexes of the galaxies to use.
-
-        Returns:
-            Astropy.table with entries corresponding to one blend.
-        """
-        number_of_objects = self.rng.integers(1, self.max_number + 1)
-        (q,) = np.where(table["ref_mag"] <= 25.3)
-
-        if indexes is None:
-            blend_table = table[self.rng.choice(q, size=number_of_objects)]
-        else:
-            blend_table = table[indexes]
-        blend_table["ra"] = 0.0
-        blend_table["dec"] = 0.0
-        if shifts is None:
-            x_peak, y_peak = _get_random_center_shift(number_of_objects, self.maxshift, self.rng)
-        else:
-            x_peak, y_peak = shifts
-        blend_table["ra"] += x_peak
-        blend_table["dec"] += y_peak
-
-        if np.any(blend_table["ra"] > self.stamp_size / 2.0) or np.any(
-            blend_table["dec"] > self.stamp_size / 2.0
-        ):
-            warnings.warn("Object center lies outside the stamp")
-
+        """Same as corresponding function for `DefaultSampling` but adds shear to output tables."""
+        blend_table = super().__call__(table, shifts, indexes)
         if isinstance(self.shear, tuple):
             blend_table["g1"] = self.shear[0]
             blend_table["g2"] = self.shear[1]
+        else:
+            raise TypeError("shear should be a tuple (g1,g2)")
         return blend_table
