35a6fdae199d550574f4972ce14992e5250a311c
thuiop <thuiop@hotmail.fr>
2022-01-21 17:42:22 +0100

    Separate noises (#254)
    * Separate the two noises
    
    * Updated tests
    
    * Possibility to choose which noise is drawn
    
    * Update doc and notebook
    
    * Changed lists to tuples
    
    * Fix test
    
    * Fix all tests
    
    * Updated notebooks
    
    * Revert change
    
    * Notebooks

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -105,7 +105,7 @@ def test_measure_kwargs(mock_show):
     )
     _, _, results = next(metrics_generator)
     average_precision = auc(results, "sep_measure", 2, plot=True)
-    assert average_precision == 0.375
+    assert average_precision == 0.4375
 
 
 def test_detection_eff_matrix():
