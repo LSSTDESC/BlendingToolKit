6e0f527e696f57e47a8a4b8b702acce060b3dfe4
b-biswas <bbiswas.dgp@gmail.com>
2022-02-16 09:32:42 +0100

    added tests with parametric sims

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -20,11 +20,28 @@ def test_reading_catsim_catalog():
 def test_reading_cosmos_catalog():
     """Returns the cosmos catalog"""
 
+    cosmos_catalog = CosmosCatalog.from_file(COSMOS_CATALOG_PATHS)
+    return cosmos_catalog
+
+
+def test_reading_cosmos_catalog_without_cuts():
+    """Returns the cosmos catalog"""
+
     cosmos_catalog = CosmosCatalog.from_file(COSMOS_CATALOG_PATHS, exclusion_level="none")
     return cosmos_catalog
 
 
 def test_getting_galsim_catalog():
+    """Returns the galsim catalog"""
+
+    cosmos_catalog = CosmosCatalog.from_file(COSMOS_CATALOG_PATHS)
+    galsim_catalog = cosmos_catalog.get_galsim_catalog()
+    # assert that the merged and galsim catalog have same length
+    assert len(galsim_catalog) == len(cosmos_catalog.table), "error reading catalog with cuts"
+    return galsim_catalog
+
+
+def test_getting_galsim_catalog_without_cuts():
     """Returns the galsim catalog"""
 
     cosmos_catalog = CosmosCatalog.from_file(COSMOS_CATALOG_PATHS, exclusion_level="none")
