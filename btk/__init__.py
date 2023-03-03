a6f562d3c979a834c0852942f5a9b00f2c305166
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2023-03-02 18:42:02 -0600

    U/im/small bug fixes (#407)
    * fix pydocstyle in toml
    
    * pydocstyle in toml
    
    * license fix
    
    * no more 3.7
    
    * no more 3.7
    
    * Empty-Commit

--- btk/__init__.py
+++ btk/__init__.py
@@ -4,10 +4,7 @@ The code generates on the fly images of overlapping parametric galaxies, while
 providing a framework to test user defined detection/deblending/measurement
 algorithms.
 """
-try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __author__ = "btk developers"
 __email__ = "imendoza@umich.edu"
