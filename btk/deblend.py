115a0aea571208fbdccc0d333a8c38b1486dcd95
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2024-04-19 14:46:49 -0400

    Last few tweaks to the quickstart & advanced-analysis tutorial (#477)
    * update poetry
    
    * add colorblind cycle
    
    * add authors and remove autoreload stuff
    
    * working on tidying up plots and adding more useful comments
    
    * fix aperture functions, fix docstrings
    
    * update packages
    
    * more ignore
    
    * use sky level instead
    
    * update git ignore with cache
    
    * fix error in tp counts
    
    * docstring
    
    * author list
    
    * wrapping up plots
    
    * more gitignore
    
    * will remove later
    
    * latest poetry from main branch
    
    * add docstring to peak_local_max
    
    * update docstring
    
    * more error catching
    
    * exclude import errors in notebooks
    
    * fix various errors in matching code
    
    There was an issue where matches containg -1 indices could be added to the final catalog or array in the Matching object (not always the non-zero matches in the front!). In the end I realized that keeping the -1 is not really useful, so I removed it altogether and array of indices matched don't contain any -1 anymore, just the true matches (accounting for distance)
    
    * we fix this given the new convention
    
    * new images after fixing matching
    
    * recall figure with new matching procedure
    
    * make blendedness robust to empty arrays from max_n_sources
    
    * all figures redone and pushed
    
    * remove cache as figures are finalized
    
    * add authors
    
    * typo
    
    * correct notebook and figure with correct blendedness definition
    
    * delete notebooks that will not make it for the release

--- btk/deblend.py
+++ btk/deblend.py
@@ -192,6 +192,7 @@ class PeakLocalMax(Deblender):
     def __init__(
         self,
         max_n_sources: int,
+        sky_level: float,
         threshold_scale: int = 5,
         min_distance: int = 2,
         use_mean: bool = False,
@@ -201,7 +202,8 @@ class PeakLocalMax(Deblender):
 
         Args:
             max_n_sources: See parent class.
-            threshold_scale: Minimum intensity of peaks.
+            sky_level: Background intensity in images to be detected (assumed constant).
+            threshold_scale: Minimum number of sigmas above noise level for detections.
             min_distance: Minimum distance in pixels between two peaks.
             use_mean: Flag to use the band average for deblending.
             use_band: Integer index of the band to use for deblending
@@ -209,6 +211,7 @@ class PeakLocalMax(Deblender):
         super().__init__(max_n_sources)
         self.min_distance = min_distance
         self.threshold_scale = threshold_scale
+        self.sky_level = sky_level
 
         if use_band is None and not use_mean:
             raise ValueError("Either set 'use_mean=True' OR indicate a 'use_band' index")
@@ -223,10 +226,15 @@ class PeakLocalMax(Deblender):
         image = np.mean(blend_image, axis=0) if self.use_mean else blend_image[self.use_band]
 
         # compute threshold value
-        threshold = self.threshold_scale * np.std(image)
+        threshold = self.threshold_scale * np.sqrt(self.sky_level)
 
         # calculate coordinates
-        coordinates = peak_local_max(image, min_distance=self.min_distance, threshold_abs=threshold)
+        coordinates = peak_local_max(
+            image,
+            min_distance=self.min_distance,
+            threshold_abs=threshold,
+            num_peaks=self.max_n_sources,
+        )
         x, y = coordinates[:, 1], coordinates[:, 0]
 
         # convert coordinates to ra, dec
@@ -387,6 +395,12 @@ class SepMultiband(Deblender):
             catalog = sep.extract(
                 band_image, self.thresh, err=bkg.globalrms, segmentation_map=False
             )
+
+            if len(catalog) > self.max_n_sources:
+                raise ValueError(
+                    "SEP predicted more sources than `max_n_sources`. Consider increasing `thresh`"
+                    " or `max_n_sources`."
+                )
 
             # convert predictions to arcseconds
             ra_detections, dec_detections = wcs.pixel_to_world_values(catalog["x"], catalog["y"])
