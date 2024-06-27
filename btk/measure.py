46d9adf4d4a19e3790269f6c8de6028d50232918
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2024-06-27 13:51:23 -0400

    Fix ksb function only (#506)
    * fix get ksb ellpiticity function by adding centroid
    
    * propagate
    
    * unnecessary #

--- btk/measure.py
+++ btk/measure.py
@@ -9,12 +9,14 @@ from galsim import GSObject
 
 
 def _get_single_ksb_ellipticity(
-    image: np.ndarray, psf: GSObject, pixel_scale: float, verbose=False
+    image: np.ndarray, centroid: np.ndarray, psf: GSObject, pixel_scale: float, verbose=False
 ) -> Tuple[float, float]:
     """Utility function to measure ellipticity using the KSB method.
 
     Args:
-        image: Image of a single, isolated galaxy with shape (H, W).
+        image: Image of a single, isolated galaxy with shape (h, w).
+        centroid: The centroid of the galaxy in the image, with shape (2,). Following the GalSim
+            convention for offsets.
         psf: A galsim object containing the PSF of the single, isolated galaxy.
         pixel_scale: The pixel scale of the galaxy image.
         verbose: Whether to print errors if they happen when estimating ellipticity.
@@ -23,33 +25,57 @@ def _get_single_ksb_ellipticity(
         Tuple of (g1, g2) containing measured shapes.
     """
     psf_image = galsim.Image(image.shape[0], image.shape[1], scale=pixel_scale)
-    psf_image = psf.drawImage(psf_image)
     gal_image = galsim.Image(image, scale=pixel_scale)
+    psf_image = psf.drawImage(psf_image)
+    pos = galsim.PositionD(centroid)
 
-    res = galsim.hsm.EstimateShear(gal_image, psf_image, shear_est="KSB", strict=False)
+    res = galsim.hsm.EstimateShear(
+        gal_image, psf_image, shear_est="KSB", strict=False, guess_centroid=pos
+    )
     output = (res.corrected_g1, res.corrected_g2)
-    if res.error_message != "" and verbose:
-        print(
-            f"Shear measurement error: '{res.error_message }'. \
-            This error may happen for faint galaxies or inaccurate detections."
-        )
+    if res.error_message != "":  # absorbs all (10, -10) and makes them np.nan
         output = (np.nan, np.nan)
+        if verbose:
+            print(
+                f"Shear measurement error: '{res.error_message }'. \
+                This error may happen for faint galaxies or inaccurate detections."
+            )
     return output
 
 
 def get_ksb_ellipticity(
-    images: np.ndarray, psf: GSObject, pixel_scale: float, verbose=False
+    images: np.ndarray, centroids: np.ndarray, psf: GSObject, pixel_scale: float, verbose=False
 ) -> np.ndarray:
-    """Return ellipticities of both true and detected galaxies, assuming they are matched."""
+    """Calculate the KSB ellipticities of a batched array of isolated galaxy images.
+
+    The galaxy images are assumed to all correspond to single band, and the input PSF is assumed
+    to be the same for all images.
+
+    If the shear measurement fails or the image is empty (no flux), then `np.nan` is returned for
+    the corresponding ellipticity.
+
+    Args:
+        images: Array of batch isolated images with shape (batch_size, max_n_sources, h, w)
+        centroids: An array of centers for each galaxy using the GalSim convention where the
+            center of the lower-left pixel is (image.xmin, image.ymin). The shape of this array is
+            (batch_size, max_n_sources, 2).
+        psf: a GalSim GSObject containing the PSF common to all galaxies.
+        pixel_scale: The pixel scale of the galaxy images.
+        verbose: Whether an error message should be printed if the ellipticity measurement fails
+            for any one of the galaxies.
+
+    Returns:
+        An array containing the measured ellipticities of shape (batch_size, max_n_sources, 2)
+    """
     # psf is assumed to be the same for the entire batch and correspond to selected band.
-    assert images.ndim == 4  # (batch_size, max_n_sources, H, W)
+    assert images.ndim == 4
     batch_size, max_n_sources, _, _ = images.shape
     ellipticities = np.zeros((batch_size, max_n_sources, 2))
     for ii in range(batch_size):
         for jj in range(max_n_sources):
             if np.sum(images[ii, jj]) > 0:
                 ellipticities[ii, jj] = _get_single_ksb_ellipticity(
-                    images[ii, jj], psf, pixel_scale, verbose=verbose
+                    images[ii, jj], centroids[ii, jj], psf, pixel_scale, verbose=verbose
                 )
             else:
                 ellipticities[ii, jj] = (np.nan, np.nan)
